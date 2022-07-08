83ce2bcd99bc0a3e4c7cd4aa24106aeeb24ead70
Abdullah Al Mamun <abdullahalmamun1804064@gmail.com>
2022-07-08 16:32:22 +0600

    Review Update and Delete and Backend end At this moment

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -124,5 +124,39 @@ exports.createProductReview = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success : true,
   })
-
 })
+
+//get reviews => /api/v1/reviews
+exports.getProductReviews = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+//Delete Product review => /api/v1/reviews
+exports.deleteReview = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+  
+  const reviews = product.reviews.filter(reviews => reviews._id.toString() !== req.query.id.toString());
+
+  const numOfReviews = reviews.length;
+  const ratings=product.reviews.reduce((acc, item) => item.rating + acc, 0) /reviews.length;
+  
+
+  await Product.findByIdAndUpdate(req.query.productId, {
+    reviews,
+    ratings,
+    numOfReviews
+  }, {
+    new: true,
+    runValidators: true,
+    useFindAndModify:false
+  })
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
